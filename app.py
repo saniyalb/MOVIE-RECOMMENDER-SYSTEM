ae4ccd786ed30c0a420af9a77570394c63ce4512
Saniya Biradar <73659530+saniyalb@users.noreply.github.com>
2022-08-25 12:34:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ from IPython.display import display
 import streamlit as st
 import pandas as pd
 import numpy as np
-#from sklearn.feature_extraction.text import TfidfVectorizer
-#from sklearn.metrics.pairwise import linear_kernel
+
 
 
 option = st.selectbox(
@@ -20,7 +19,7 @@ type_movies=movies.groupby("genres")["movieId"].sum().sort_values(ascending=Fals
 
 merged_left = pd.merge(left=movies, right=ratings, how='left', left_on='movieId', right_on='movieId')
 if option=='Popularity-Based Recommender System':
-     ge=st.text_input("Genre(g):","Comedy")
+     ge=st.text_input("Genre(g):","Adventure|Children|Fantasy")
      th=st.text_input("Minimum reviews threshold(t):",100)
      re=st.text_input("Num recommendations (N) :",5)
      out=merged_left[merged_left["genres"]==ge ].sort_values(by=["genres","rating","userId"], ascending=False)
@@ -32,16 +31,14 @@ if option=='Popularity-Based Recommender System':
      final=out[["Movie Title","Average Movie Rating","Num Reviews"]]
      st.write(final.head(int(re)))
 elif option=='Content-Based Recommender System':
-     #cv=TfidfVectorizer()
-     #tfidf_matrix=cv.fit_transform(movies['genres'])
-     #cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+    
      for i in range(0,len(movies)):
           Str = movies["title"][i]
           l = len(Str)
           Remove_last = Str[:50]
           movies["title"][i]=Remove_last
      n_movies=movies
-     mv=st.text_input("Movie Title (t): ",'Jumanji')
+     mv=st.text_input("Movie Title (t): ",'Father of the Bride Part II (1995)')
      rec=st.text_input("Num recommendations (N):",5)
      movie=n_movies[n_movies["title"]==mv]
      id1=movie["movieId"].tolist()
@@ -54,7 +51,7 @@ elif option=='Content-Based Recommender System':
      out2=out2.reset_index(drop=True).title.head(int(rec))
      st.write(out2)
 else:
-     usr=st.text_input("UserID:",1)
+     usr=st.text_input("UserID:",47)
      rec=st.text_input("Num recommendations(N):",10)
      thr=st.text_input("Threshold for similar users (k):",100)
      usr_mv=merged_left[merged_left["userId"]==int(usr)][["title","userId"]]
