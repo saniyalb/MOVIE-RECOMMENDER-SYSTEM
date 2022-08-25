fdc1b072a23e15d1ea519db84c906dafd264885f
Saniya Biradar <73659530+saniyalb@users.noreply.github.com>
2022-08-25 11:51:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from IPython.display import display
 import streamlit as st
 import pandas as pd
 import numpy as np
+import sklearn
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 
